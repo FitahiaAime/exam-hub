d490b66ef96ab150d5c76e7f7e72ae09bf1254cd
Tianaluciano-arch <hei.luciano.3@gmail.com>
2026-08-26 11:21:39 +0300

    Ajout du stockage du token dans le navigateur

---
+++ exam-hub/src/services/fetchWithAuth.js
@@ -0,0 +1,8 @@
+const TOKEN_KEY = 'exam-hub-token';
+const BASE_URL = '/api';
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const setToken = (token) => localStorage.setItem(TOKEN_KEY, token);
+export const removeToken = () => localStorage.removeItem(TOKEN_KEY);
+
+
